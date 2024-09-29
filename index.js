fd2c5fd06756c9e5c1db44cd29c63925184de7ac
Bajorum <Bilal@Youssef.ca>
2024-09-29 19:51:34 -0400

    Updated code with GET /hello, GET /user, and POST /user routes

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// GET /hello route
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to the homepage!</h1>
+    <p><a href="/hello">Go to /hello</a></p>
+    <p><a href="/user?firstname=John&lastname=Doe">Go to /user with query params (John Doe)</a></p>
+  `);
+});
+
+
 app.get('/hello', (req, res) => {
   res.send('Hello Express JS');
 });
 
-// GET /user route with query parameters
+
 app.get('/user', (req, res) => {
   const firstname = req.query.firstname || 'Pritesh';
   const lastname = req.query.lastname || 'Patel';
   res.json({ firstname, lastname });
 });
 
-// POST /user route with path parameters
+
 app.post('/user/:firstname/:lastname', (req, res) => {
   const { firstname, lastname } = req.params;
   res.json({ firstname, lastname });
 });
 
-// Start the server
+
+app.use((req, res) => {
+  res.status(404).send('Sorry, route not found!');
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
